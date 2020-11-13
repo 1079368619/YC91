34e51813a7bafb8123b6a5ef6ca81888b31c0a13
1079368619 <1079368619@qq.com>
2020-11-13 15:11:46 +0800

    添加

--- C91-S2-Ply-javaee/src/com/yc/javaee/servlet/DemoServlet.java
+++ C91-S2-Ply-javaee/src/com/yc/javaee/servlet/DemoServlet.java
@@ -1,26 +1,24 @@
 package com.yc.javaee.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello.s")
+@WebServlet("/DemoServlet")
 public class DemoServlet extends HttpServlet {
-	
 	private static final long serialVersionUID = 1L;
-    
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//
-		response.setContentType("text/html; charset=utf-8");
-		//
-		PrintWriter out = response.getWriter();
-		out.print("<h1>Hello World</h1>");
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }
